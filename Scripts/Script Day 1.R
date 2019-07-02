11c9b1383e78b5afffb89b123e6dceace596a90d
Yoonison <yoonmatthew@gmail.com>
2019-07-02 15:19:52 +0200

    Version 2

--- Scripts/Script Day 1.R
+++ Scripts/Script Day 1.R
@@ -3,6 +3,6 @@ library(tidyverse)
 mpg
 
 ggplot(data = mpg) +
-  geom_point(mapping = aes(x = displ, y = hwy, color = displ < 5  ))
-
+  geom_point(mapping = aes(x = displ, y = hwy)) +
+  facet_grid(drv ~ cyl)
 ?geom_point

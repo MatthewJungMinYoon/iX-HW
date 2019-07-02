c8afc74a87aafc2ccaeb1db4ebb5cdfb03d555ca
Yoonison <yoonmatthew@gmail.com>
2019-07-02 15:39:59 +0200

    3.6 ex 1

--- Scripts/Script Day 1.R
+++ Scripts/Script Day 1.R
@@ -2,7 +2,26 @@ library(tidyverse)
 
 mpg
 
-ggplot(data = mpg) +
-  geom_point(mapping = aes(x = displ, y = hwy)) +
-  facet_grid(drv ~ cyl)
-?geom_point
+#3.6.1
+#use geom_line for line chart, geom_boxplot for boxplot, geom_histogram for histogram, geom_area for area
+
+#Line chart
+mpg %>%
+  group_by(year) %>%
+  summarise(m = mean(cty)) %>%
+  ggplot(aes(year, m)) +
+  geom_line()
+
+#Boxplot
+ggplot(mpg, aes(class, hwy)) +
+  geom_boxplot()
+
+#Histogram
+ggplot(mpg, aes(displ))+
+  geom_histogram(bins = 60)
+
+#Area
+huron <- data.frame(year = 1875:1972, level = as.vector(LakeHuron))
+ggplot(huron, aes(year, level)) +
+  geom_area()
+?mpg
